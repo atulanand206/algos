43667f529bbe97512f0bd9cf3b41c22345cb71fc
Atul Anand <atulanand206@gmail.com>
2021-06-04 01:27:57 +0530

    :joy: Canvas with Quad Curve.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import logo from './logo.svg';
+import  { useRef, useEffect } from 'react';
 import './App.css';
+import { drawCurves } from './curves';
+
+const drawStar = (cx) => {
+  function pointAt(center, radius, angle) {
+    const sinAngle = Math.sin(angle)
+    const cosAngle = Math.cos(angle)
+    return [center[0] + cosAngle * radius, center[1] + sinAngle * radius]
+  }
+
+  var center = [200, 200]
+  var rad = 100
+  var start = pointAt(center, rad, 0)
+  cx.beginPath();
+  cx.moveTo(start[0], start[1])
+  for (var i = 1; i <= 8; i++) {
+    var point = pointAt(center, rad, i * Math.PI / 4)
+    cx.quadraticCurveTo(center[0], center[1], point[0], point[1])
+  }
+  cx.moveTo(start[0], start[1])
+  cx.fillStyle = "#FF8C00"
+  cx.fill()
+  cx.fillStyle = "transparent"
+}
+
+const drawZigZag = (cx) => {
+  function nextPoint(point) {
+    if (point[0] === 10) return [100, point[1] + 8]
+    else return [10, point[1] + 8]
+  }
+  var start = [10, 10]
+  cx.beginPath();
+  cx.moveTo(start[0], start[1])
+  for (var i = 0; i < 12; i++) {
+    start = nextPoint(start)
+    cx.lineTo(start[0], start[1])
+    cx.strokeStyle = "#000";
+    cx.stroke()
+  }
+}
+
+const Canvas = props => {
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+    const size = 800;
+    var scale = window.devicePixelRatio; // Change to 1 on retina screens to see blurry canvas.
+    canvas.width = size * scale;
+    canvas.height = size * scale;
+    context.scale(scale, scale);
+    drawCurves(context)
+    // drawZigZag(context)
+    // drawStar(context)
+  }, [])
+
+  return <canvas ref={canvasRef} {...props}/>
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Canvas className='canvas-wrapper'/>
       </header>
     </div>
   );
---
+++ src/curves.jsx
@@ -0,0 +1,86 @@
+export function drawCurves(ctx) {
+    ctx.beginPath();
+    var percentPts = []
+    for (var i = -1; i <= 8; i++) {
+        const pts = getPoints({ x: 0, y: 100 + i * 100 }, 8, ptHor, controlHor)
+        // drawCurve(ctx, pts)
+        percentPts = percentPts.concat(percentPoints(pts))
+    }
+    for (var j = -1; j <= 8; j++) {
+        const pts = getPoints({ x: 100 + j * 100, y: 0 }, 8, ptVer, controlVer)
+        // drawCurve(ctx, pts)
+        percentPts = percentPts.concat(percentPoints(pts))
+    }
+    console.log(percentPts)
+    var k = 0;
+    const f = setInterval(() => {
+        if (k == percentPts.length - 1) clearInterval(f)
+        drawPoint(ctx, percentPts[k++])
+    }, 5)
+}
+
+function drawPoint(ctx, point) {
+    if (point.x < 0 || point.y < 0 || point.x > 800 || point.y > 800) return
+    ctx.beginPath();
+    ctx.arc(point.x, point.y, 5, 0, Math.PI * 2, false);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function drawCurve(ctx, values) {
+    const points = values[0]
+    const controlPoints = values[1]
+    ctx.moveTo(points[0].x, points[0].y);
+    for (var i = 1; i < points.length; i++) {
+        const point = points[i]
+        const control = controlPoints[i]
+        ctx.quadraticCurveTo(control.x, control.y, point.x, point.y)
+        ctx.strokeStyle = "#000"
+        ctx.stroke()
+    }
+}
+
+function percentPoints(points) {
+    const pts = []
+    for (var j = 1; j < points.length; j++)
+        for (var k = 0; k <= 1; k += 0.01)
+            pts.push(getQuadraticBezierXYatPercent(points[0][j - 1], points[1][j], points[0][j], k));
+    return pts
+}
+
+function getPoints(start, count, pointFunc, controlFunc) {
+    var pt = start
+    const arr = []
+    const mArr = [[0, 0]]
+    for (var i = 0; i < count; i++) {
+        const last = pt;
+        arr.push(pt)
+        pt = pointFunc(pt)
+        const mid = controlFunc(pt, last, i)
+        mArr.push(mid)
+    }
+    arr.push(pt)
+    return [arr, mArr]
+}
+
+function getQuadraticBezierXYatPercent(startPt, controlPt, endPt, percent) {
+    var x = Math.pow(1 - percent, 2) * startPt.x + 2 * (1 - percent) * percent * controlPt.x + Math.pow(percent, 2) * endPt.x
+    var y = Math.pow(1 - percent, 2) * startPt.y + 2 * (1 - percent) * percent * controlPt.y + Math.pow(percent, 2) * endPt.y
+    return { x: x, y: y }
+}
+
+function ptHor(pt) {
+    return { x: pt.x + 100, y: pt.y }
+}
+
+function ptVer(pt) {
+    return { x: pt.x, y: pt.y + 100 }
+}
+
+function controlHor(pt, last, i) {
+    return { x: (pt.x + last.x) / 2, y: pt.y + ((i % 2 === 0) ? 1 : -1) * 100 };
+}
+
+function controlVer(pt, last, i) {
+    return { x: pt.x + ((i % 2 === 0) ? 1 : -1) * 100, y: (pt.y + last.y) / 2 }
+}
