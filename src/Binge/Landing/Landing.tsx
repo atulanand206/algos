3cf7997bb8077305d8e148baae91e8c98b7e1e9f
Atul <atulanand206@gmail.com>
2021-08-25 02:25:02 +0530

    Basic landing scene.

--- src/Binge/Landing/Landing.tsx
+++ src/Binge/Landing/Landing.tsx
@@ -1,8 +1,27 @@
+import { useState } from 'react';
+import Scoreboard from './../Scoreboard/Scoreboard';
+import './Landing.scss'
+ 
 const Landing = () => {
+  
+  const [visibilityScoreBoard, setVisibilityScoreboard] = useState(false);
+
+  const openScoreboard = () => {
+    setVisibilityScoreboard(true)
+  }
+
+  const closeScoreboard = () => {
+    setVisibilityScoreboard(false)
+  }
 
   return (
-    <div>
-A
+    <div className='landing__wrapper'>
+      <p className='landing__logo'>Binguiz</p>
+      <div className='landing__controls'>
+        <button className='landing__controls-button' onClick={openScoreboard}>Let's Begin</button>
+        <button className='landing__controls-button' onClick={closeScoreboard}>Scoreboard</button>
+      </div>
+      <Scoreboard visibility={visibilityScoreBoard}/>
     </div>
   )
 }
--- src/Binge/Scoreboard/Scoreboard.tsx
+++ src/Binge/Scoreboard/Scoreboard.tsx
@@ -1,8 +1,17 @@
-const Scoreboard = () => {
+import classNames from 'classnames/bind'
+import './Scoreboard.scss'
+
+type Props = {
+  visibility: Boolean;
+}
+
+const Scoreboard = (props: Props) => {
 
   return (
-    <div>
-
+    <div className={classNames(
+      'scoreboard__wrapper', 
+      props.visibility && 'scoreboard__wrapper-visible')} >
+      Babes
     </div>
   )
 }
