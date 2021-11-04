7f424a883206e75756ad330ecdacf89b39b7427b
Atul Anand <atulanand206@gmail.com>
2021-11-04 17:15:39 +0530

    removed a couple of unused methods.

--- src/Binge/v1/dataStore/DataStore.tsx
+++ src/Binge/v1/dataStore/DataStore.tsx
@@ -1,14 +1,6 @@
 import { ROLE_PLAYER } from "../features/Features";
 import { Player, Snap, Team } from '../utils/_interfaces';
 
-export const quizId = (snap: any) => {
-  return snap.snapshot.quiz_id
-}
-
-export const questionId = (snap: any) => {
-  return snap.snapshot.question_id
-}
-
 export const getPlayersTeamId = (role: string, snapshot: Snap, playerId: string) => {
   if (role === ROLE_PLAYER) {
     var tems = snapshot.teams.filter((team: Team) => team.players.filter((playr: Player) => playr.id === playerId).length === 1)
